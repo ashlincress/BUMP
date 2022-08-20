e3c88b40169549d20ee0651c8891f0195addba72
ashlincress <102940783+ashlincress@users.noreply.github.com>
2022-08-20 09:12:33 +0530

    glad

---
+++ app/src/main/java/com/example/bump/CounsMainPage.java
@@ -0,0 +1,14 @@
+package com.example.bump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CounsMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_couns_main_page);
+    }
+}
